7193881a66a0a72e101a26bb19fe2462b36e6fa7
Justin Poh <e0031499@u.nus.edu>
2018-08-30 20:42:18 +0800

    Input module completed.

--- src/com/company/Input.java
+++ src/com/company/Input.java
@@ -1,4 +1,20 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Input {
+    public List<String> readAllLines() {
+        Scanner input = new Scanner(System.in);
+        List<String> allLines = new ArrayList<String>();
+        String currentLine = input.nextLine().trim();
+
+        while(!currentLine.isEmpty()) {
+            allLines.add(currentLine);
+            currentLine = input.nextLine().trim();
+        }
+
+        return allLines;
+    }
 }
