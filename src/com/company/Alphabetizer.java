54d20dc3ccc72905ab96d0566b44a74dc65da4dd
Justin Poh <e0031499@u.nus.edu>
2018-08-30 21:00:19 +0800

    Alphabetizer module.

--- src/com/company/Alphabetizer.java
+++ src/com/company/Alphabetizer.java
@@ -1,4 +1,19 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Alphabetizer {
+
+    private List<String> shiftedLines;
+
+    public Alphabetizer(List<String> shiftedLinesInput) {
+        shiftedLines = new ArrayList<String>(shiftedLinesInput);
+    }
+
+    public List<String> getSortedLines() {
+        Collections.sort(shiftedLines);
+        return shiftedLines;
+    }
 }
