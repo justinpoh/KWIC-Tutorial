654c2d2d92c786f93e20f2d3918807be301ac77e
caoliangnus <liangcao1213@yahoo.com>
2018-08-31 17:07:07 +0800

    Process line by line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,34 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        //Pipes and Filter architecture
 
         //Initializing pipes and filters
         Pipe pipe1 = new Pipe();
         Pipe pipe2 = new Pipe();
         Pipe pipe3 = new Pipe();
 
-        Input inputFilter = new Input(pipe1);
         CircularShifter circularShifterFilter = new CircularShifter(pipe1, pipe2);
         Alphabetizer alphabetizerFilter = new Alphabetizer(pipe2, pipe3);
         Output outputFilter = new Output(pipe3);
 
-        inputFilter.readAllLines();
-        circularShifterFilter.circularShift();
-        alphabetizerFilter.sortLines();
-        outputFilter.printAllSortedLines();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please type in the line(s):");
+
+
+        while(input.hasNextLine()) {
+            String currentLine = input.nextLine().trim();
+            pipe1.sendData(currentLine);
+            circularShifterFilter.circularShift();
+            alphabetizerFilter.sortLines();
+            outputFilter.printAllSortedLines();
+        }
+
     }
 }
